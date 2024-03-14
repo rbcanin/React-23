583c8380a2cb7480858a5c1e98df0c4a9028fa70
Rebeca Soares <rebeca.soares.canin@gmail.com>
2024-03-14 17:11:09 -0300

    mais um

--- 7_PROJECT/project/src/App.jsx
+++ 7_PROJECT/project/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
+const url = "http://localhost:3000/products/";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
+  const handleSubmit = async () => {};
 
   return (
     <>
       <div className="App">
-        <h1>Sessão 3</h1>
+        <h1>Lista de Produtos</h1>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {" "}
+              {product.name} - R$: {product.price}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
