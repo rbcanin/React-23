78d54242272980f54eab32099c70ae8832225d80
Rebeca Soares <rebeca.soares.canin@gmail.com>
2024-03-28 15:43:59 -0300

    2

--- 7_PROJECT/project/src/App.jsx
+++ 7_PROJECT/project/src/App.jsx
@@ -47,6 +47,7 @@ function App() {
       <div className="App">
         <h1>Lista de Produtos</h1>
         <h1>oi</h1>
+        <h2>oioi</h2>
         <ul>
           {products.map((product) => (
             <li key={product.id}>
