2e2782dc646708c0f316f860d21197f806089c0f
Rebeca Soares <rebeca.soares.canin@gmail.com>
2024-04-04 17:19:09 -0300

    hook

--- 10_HOOKS/hooks/src/components/HookUseReducer.jsx
+++ 10_HOOKS/hooks/src/components/HookUseReducer.jsx
@@ -5,10 +5,14 @@ const HookUseReducer = () => {
     return Math.random(state);
   });
 
+  //avançando
+
   return (
     <div>
       {/*useReducer executa a função na hora da alteração, manipular valores em casos mais complexos */}
       <h2>useReducer</h2>
+      <p>numero: {numero}</p>
+      <button onClick={dispatch}>Alterar número</button>
       <hr />
     </div>
   );
